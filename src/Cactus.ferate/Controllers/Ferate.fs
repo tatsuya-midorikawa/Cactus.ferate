03a4742b2f6b54b614250af85422eae6273aef02
midoliy <midoliy@outlook.jp>
2023-02-04 22:07:45 +0900

    update

--- src/Cactus.ferate/Controllers/Ferate.fs
+++ src/Cactus.ferate/Controllers/Ferate.fs
@@ -11,28 +11,9 @@ open Cactus.ferate
 [<ApiController>]
 [<Route("[controller]")>]
 type FerateController (logger : ILogger<_>) =
-    inherit ControllerBase()
+  inherit ControllerBase()
 
-    let summaries =
-        [|
-            "Freezing"
-            "Bracing"
-            "Chilly"
-            "Cool"
-            "Mild"
-            "Warm"
-            "Balmy"
-            "Hot"
-            "Sweltering"
-            "Scorching"
-        |]
-
-    [<HttpGet>]
-    member _.Get() =
-        let rng = System.Random()
-        [|
-            for index in 0..4 ->
-                { Date = DateTime.Now.AddDays(float index)
-                  TemperatureC = rng.Next(-20,55)
-                  Summary = summaries.[rng.Next(summaries.Length)] }
-        |]
+  [<HttpGet>]
+  member _.Get() =
+    let csv = Ferate.fetch'historical'data()
+    csv
--- src/Cactus.ferate/Core/Ferate.fs
+++ src/Cactus.ferate/Core/Ferate.fs
@@ -1,4 +1,40 @@
 namespace Cactus.ferate
 
+open System
+open System.Net.Http
+open System.Threading.Tasks
+open System.Text
+open System.IO
+
 module Ferate =
-  let a = ()
+  let now () = DateOnly.FromDateTime DateTime.Now
+
+  [<Literal>]
+  let private ep = "https://www.mizuhobank.co.jp/market/csv/quote.csv"
+  let private client = new HttpClient()
+  let private enc =
+    Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+    Encoding.GetEncoding "Shift_JIS"
+  let mutable private last'updated =  DateOnly.FromDateTime DateTime.MinValue
+  let mutable private historical'data: string[] = [||]
+
+
+  let monitor = Object()
+
+  let fetch'historical'data () = 
+    lock monitor (fun () ->
+      if last'updated < now() then
+        task { 
+          let! r = client.GetAsync ep
+          use! s = r.Content.ReadAsStreamAsync()
+          use ms = new MemoryStream()
+          do! s.CopyToAsync(ms)
+          let csv = enc.GetString(ms.ToArray())
+          historical'data <- (csv.Split "\n")[2..]
+          historical'data[0] <- $"DATE{historical'data[0]}"
+          // let! res = client.GetStringAsync ep
+          // historical'data <- res.Split "\r\n"
+        }
+        |> Task.WaitAll
+      historical'data
+    )
