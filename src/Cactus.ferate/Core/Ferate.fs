5f165fda83049de62d765508bb8840581d585e28
midoliy <midoliy@outlook.jp>
2023-02-05 08:55:31 +0900

    wip

--- src/Cactus.ferate/Core/Ferate.fs
+++ src/Cactus.ferate/Core/Ferate.fs
@@ -7,7 +7,7 @@ open System.Text
 open System.IO
 
 type Ferate = {
-  date: DateOnly option
+  DATE: DateOnly option
   USD: decimal option
   GBP: decimal option
 }
@@ -27,7 +27,12 @@ module Ferate =
   let private date'parse (s: string) = match System.DateOnly.TryParse s with true, v -> Some v | _ -> None
   let private decimal'parse (s: string) = match System.Decimal.TryParse s with true, v -> Some v | _ -> None
 
-  let monitor = Object()
+  let private monitor = Object()
+  let private idx = Map[ 
+    ("DATE", 0); ("USD",  1); ("GBP",  2); ("EUR",  3); ("CAD",  4); ("CHF",  5); ("SEK",  6); ("DKK",  7); ("NOK",  8); ("AUD",  9);
+    ("NZD", 10); ("ZAR", 11); ("BHD", 12); ("IDR", 13); ("CNY", 14); ("HKD", 15); ("INR", 16); ("MYR", 17); ("PHP", 18); ("SGD", 19);
+    ("KRW", 20); ("THB", 21); ("KWD", 22); ("SAR", 23); ("AED", 24); ("MKN", 25); ("PGK", 26); ("HUF", 27); ("CZK", 28); ("PLN", 29);
+    ("TRY", 30); ]
 
   let private fetch'historical'data () = 
     lock monitor (fun () ->
@@ -46,9 +51,9 @@ module Ferate =
             |> Array.map (fun d ->
               let xs = d.Split ','
               try
-                { date = date'parse xs[0]; USD = decimal'parse xs[1]; GBP = decimal'parse xs[2] }
+                { DATE = date'parse xs[0]; USD = decimal'parse xs[1]; GBP = decimal'parse xs[2] }
               with 
-                | e -> System.Diagnostics.Debug.WriteLine $"### {e.Message}"; { date = None; USD = None; GBP = None } )
+                | e -> System.Diagnostics.Debug.WriteLine $"### {e.Message}"; { DATE = None; USD = None; GBP = None } )
         }
         |> Task.WaitAll
         last_updated <- now()
